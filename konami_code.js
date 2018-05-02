f35bfbccabb5ca0592284886e0e2d7f976dccd65
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-02 00:30:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const input = document.querySelector('body')
 
-  input.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e) {
     parseInt(e.detail || e.which)
   })
 }
