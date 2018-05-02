1da24ebfa3b917e92fad8fe812aa94620a041d3c
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-02 00:35:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,10 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const input = document.querySelector('body')
-
+  var index = 0
   input.addEventListener('keydown', function(e) {
 
-    if (parseInt(e.detail || e.which) == 38) {
+    if (parseInt(e.detail || e.which) == code[index]) {
       alert(`Congrats!`)
     }
   })
